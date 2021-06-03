051256ec78d4babecdfa41d824f553c3691620f9
ardhisr1 <radika.kerenz1@gmail.com>
2021-06-03 21:18:42 +0700

    api done for now

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	r := gin.Default()
 
-	// routes.UserRoute(r)
-	// routes.UserDetailRoute(r)
-	// routes.UserTransactionRoute(r)
-	// routes.SubTypeRoute(r)
-	// routes.CategoryRoute(r)
-	// routes.MasteryRoute(r)
-	// routes.CoursesRoute(r)
+	routes.UserRoute(r)
+	routes.UserDetailRoute(r)
+	routes.UserTransactionRoute(r)
+	routes.SubTypeRoute(r)
+	routes.CategoryRoute(r)
+	routes.MasteryRoute(r)
+	routes.CoursesRoute(r)
 	routes.CryptoRoute(r)
 
 	r.Run(":8080")
